d822551e8edbcdc76a3ca9925fef3caffb39c5ad
hiddevuijk <hdvuijk@gmail.com>
2016-01-20 18:23:53 +0100

    added basic headers

---
+++ headers/read_input.h
@@ -0,0 +1,40 @@
+#ifndef GUARD_read_input_h
+#define GUARD_read_input_h
+
+#include <fstream>
+#include <string>
+
+
+std::string remove_text(std::string str)
+{
+	std::size_t pos = str.find('=');
+	return str.substr(pos+1);
+}
+
+void read_input(int& N, double& mean, double& var,int& seed,std::string infile_name)
+{
+	ifstream input(infile_name);
+	std::string temp;
+
+	std::getline(input,temp);
+	temp = remove_text(temp);
+	N = std::stoi(temp);
+
+	std::getline(input,temp);
+	temp = remove_text(temp);
+	mean = std::stod(temp);	
+
+	std::getline(input,temp);
+	temp = remove_text(temp);
+	var = std::stod(temp);
+
+	std::getline(input,temp);
+	temp = remove_text(temp);
+	seed = std::stoi(temp);
+}
+
+
+
+#endif
+
+
